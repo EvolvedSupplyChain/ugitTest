686eef7068b0eb2c9bf2c7b8c2d2a0b3c02d5a27
EvolvedSupplyChain <161888284+EvolvedSupplyChain@users.noreply.github.com>
2024-04-01 11:00:10 -0700

    Update logger.py

--- logger.py
+++ logger.py
@@ -3,15 +3,15 @@ import time
 
 def log_data():
     print("Attempt number 1")
-    led = machine.Pin('LED', machine.Pin.OUT)
+    led = machine.Pin(10, machine.Pin.OUT)
         
     while True:
         try:
-            led.toggle()
+            led.on()
         except Exception as error:
             print(error)
             
         print("5555")
         time.sleep(1)
-    
+        led.off()
     return
